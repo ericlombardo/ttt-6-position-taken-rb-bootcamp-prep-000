4ad3f2ac0a1f8703bdcc8f7ce6a8020077051814
Eric Lombardo <eric.m.lombardo@gmail.com>
2020-09-30 21:54:19 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,10 @@ def position_taken?(board, index)
   # checking to see if the spot empty
   if board[index] == " " || "" || nil
     false
+  elsif board[index] == ("")
+    false
+  elsif board[index] == (nil)
+    false
   elsif board[index] == "X" || "O"
     true
   end
