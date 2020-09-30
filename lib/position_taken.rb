7aa42b755adf354313804c162b9af3a1b5a797c6
Eric Lombardo <eric.m.lombardo@gmail.com>
2020-09-30 21:53:21 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -7,9 +7,7 @@ def position_taken?(board, index)
     false
   elsif board[index] == (nil)
     false
-  elsif board[index] == ("X")
-    true
-  elsif board[index] == ("O")
+  elsif board[index] == ("X" || "O")
     true
   end
 end
