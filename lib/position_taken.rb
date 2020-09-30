6bc7eaa546a0f6a5574492a73bedd488549b7066
Eric Lombardo <eric.m.lombardo@gmail.com>
2020-09-30 22:08:58 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  # checking to see if the spot is an empty string
-  if board[index] == " " || "" || nil
+  if board[index] == " "
     false
-  # check to see if there is an "X" or an "O"
-  elsif board[index] == "X" || "O"
-    true
   end
 end
